4e07b6b8e88c1b0ed60ef039a070e535ebc2e9d6
Ross Whitehead <rossshaggywhitehead@hotmail.com>
2019-03-02 16:28:21 +0000

    Fix tests

--- test/route-tests.js
+++ test/route-tests.js
@@ -1,16 +1,30 @@
 'use strict'
 
-var request = require('supertest'),
-    app = require('../app').getApp
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const app = require('../app')
+const should = chai.should()
 
-describe('GET /', function(){
-    it('expects HTTP response 200', function(done){
-        request(app).get('/').expect(200, done)
+chai.use(chaiHttp)
+
+describe('GET /', () => {
+    it('it should respond with HTTP 200', (done) => {
+    chai.request(app)
+        .get('/')
+        .end((err, res) => {
+            res.should.have.status(200)
+            done()
+        })
     })
 })
 
-describe('GET /users', function(){
-    it('expects HTTP response 200', function(done){
-        request(app).get('/users').expect(200, done)
+describe('GET /users', () => {
+    it('it should respond with HTTP 200', (done) => {
+    chai.request(app)
+        .get('/users')
+        .end((err, res) => {
+            res.should.have.status(200)
+            done()
+        })
     })
 })
