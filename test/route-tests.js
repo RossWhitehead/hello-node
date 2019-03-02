7226f0aeddf04796f64bace33abc206f79f42331
Ross Whitehead <rossshaggywhitehead@hotmail.com>
2019-03-02 14:37:39 +0000

    Create project

---
+++ test/route-tests.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var request = require('supertest'),
+    app = require('../app').getApp
+
+describe('GET /', function(){
+    it('expects HTTP response 200', function(done){
+        request(app).get('/').expect(200, done)
+    })
+})
+
+describe('GET /users', function(){
+    it('expects HTTP response 200', function(done){
+        request(app).get('/users').expect(200, done)
+    })
+})
